102b8b8e6839bcbf540cba0a19d9693f2dc92a6a
Hgxegz1 <hgxegz21@gmail.com>
2021-12-18 10:43:36 -0800

    added the function to get top categories in area

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from functions import *
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+
+    #after the user inputs the category use this function
+    @app.route('/getCats')
+    def catergories():
+        location = request.args.get('l')
+        term = request.args.get('t')
+        return returnCats(location, term)
 
     @app.route('/api')
     def api():
