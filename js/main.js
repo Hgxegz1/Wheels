49f291563f7642309874de1fcd2ad2cec98bdf5e
Hgxegz1 <hgxegz21@gmail.com>
2021-12-18 11:37:44 -0800

    added function in JS..need to test if it works

--- js/main.js
+++ js/main.js
@@ -19,3 +19,15 @@ function getDataFromServer2() {
         $("#testHeader2").html($("#input-box2").val())
     })
 }
+
+function getCategoryForWheel(location, term) {
+    fetch('http://127.0.0.1:5000/getCats?l='+str(location)+"&t="+str(input2))
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log("working")
+        $("#testHeader2").html($("#input-box2").val())
+    })
+}
+
