b18bfb9ab7563bd4a3676311f6e7e3252b6af38b
Raul Cruz <raulcollege2@yahoo.com>
2021-12-19 01:52:03 -0800

    Allow dynamic text placing on page 3

--- js/main.js
+++ js/main.js
@@ -1,27 +1,5 @@
-function getDataFromServer() {
-    fetch('https://ac4c-76-91-41-187.ngrok.io/api')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(myJson);
-        $("#testHeader").html($("#input-box").val())
-    })
-}
-
-function getDataFromServer2() {
-    fetch('https://ac4c-76-91-41-187.ngrok.io/api2')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log("working")
-        $("#testHeader2").html($("#input-box2").val())
-    })
-}
-
 function sendRequest() {
-    setChoice()
+    setChoice();
     loc = localStorage.getItem('location')
     cho = localStorage.getItem('choice')
     fetch('https://ac4c-76-91-41-187.ngrok.io/getCats?l='+loc+'&t='+cho,
