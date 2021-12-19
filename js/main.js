c7a4409911adf020c1a7451c21d47a3f1382e0c4
EASantos21 <easantos998@gmail.com>
2021-12-18 21:04:05 -0800

    New branch

--- js/main.js
+++ js/main.js
@@ -20,14 +20,12 @@ function getDataFromServer2() {
     })
 }
 
-function getCategoryForWheel(location, term) {
-    fetch('https://ac4c-76-91-41-187.ngrok.io/getCats?l='+str(location)+"&t="+str(input2))
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log("working")
-        $("#testHeader2").html($("#input-box2").val())
-    })
+function getLocation() {
+    let location = $('#location').val();
+    console.log(location)
+    /*fetch('https://ac4c-76-91-41-187.ngrok.io/getCats?l='+location+'&t=Food',
+    {"method": "GET",
+     "headers": {}})
+        .then(response => response.json())
+        .then(data => console.log(data)); */
 }
-
